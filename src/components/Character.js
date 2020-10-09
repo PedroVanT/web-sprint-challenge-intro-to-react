7d83600711b1decb0973cb6354440766bb7330ad
Pedro Van Thienen <pedrovanthienen@gmail.com>
2020-10-09 16:09:19 -0400

    done

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,22 +1,20 @@
 import React from "react";
 import styled from "styled-components";
-
+import image from "../images/spacePic.jpg";
 // Write your Character component here
 
 const CharacterCard = styled.div`
   display: flex;
   justify-content: space-between;
-  margin-right: 10%;
-  margin-left: 10%;
+  margin-right: 18%;
+  margin-left: 18%;
   margin-bottom: 5%;
   border: 3px outset yellow;
   padding: 5px;
-  background: black;
+  background-image: url(image);
   background-clip: border-box;
-  /* background-clip: padding-box; */
   color: yellow;
   font-size: 1.2rem;
-
   h3 {
     margin-left: 3%;
   }
@@ -29,7 +27,7 @@ const Character = (props) => {
   const { characterInfo } = props;
 
   return (
-    <CharacterCard>
+    <CharacterCard style={{ backgroundImage: `url(${image})` }}>
       <h3>{characterInfo.name}</h3>
       <h5>Born in the year: {characterInfo.birth_year}</h5>
     </CharacterCard>
